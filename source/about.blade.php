6b763e2edb10636df1da1d1b0a9b8a6839f747d1
Andre Lewis <amalkong@gmail.com>
2021-10-04 04:32:49 -0500

    Update about.blade.php

--- source/about.blade.php
+++ source/about.blade.php
@@ -6,11 +6,20 @@
     <h1>About</h1>
 
     <p>My name is {{ $page->owner->name }}</p>
-
+    
+    <p>This is a showcase for all my projects outeside of github</p>
+    
+    <h2>Project List:</h2>
+    
+    <ul>
+        <li><a href="https://amalkong.github.io/css-filter/">Css Filter</a></li>
+    </ul>
+    
     <h2>Links:</h2>
 
     <ul>
-        <li><a href="/twitter" target="_blank">Twitter</a></li>
-        <li><a href="/github" target="_blank">GitHub</a></li>
+        <li><a href="https://twitter.com/amalkong" target="_blank">Twitter</a></li>
+        <li><a href="https://github.com/amalkong" target="_blank">GitHub</a></li>
+        <li><a href="https://stackoverflow.com/users/15254982/amalkong" target="_blank">Stack Overflow</a></li>
     </ul>
 @endsection
