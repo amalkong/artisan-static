b76f9b06b0bf0f678bcd7e6d32a4e59e35bc7d0d
Andre Lewis <amalkong@gmail.com>
2021-10-15 11:49:12 -0500

    add mobile menu

--- source/_partials/menu.blade.php
+++ source/_partials/menu.blade.php
@@ -1,10 +1,12 @@
 			<nav class="main-nav">
+				<button class="nav-menu-button" onclick="document.querySelector('.main-nav').classList.toggle('toggled');"><i class="fa fa-bars"></i></button>
+				<button class="nav-close-button" onclick="document.querySelector('.main-nav').classList.remove('toggled');"><i class="fa fa-times"></i></button>
 				<ul>
-					<li><a class="fancy-anchor {{ $page->selected('home') }}" href="/">Home</a></li>
+					<li><a class="fancy-anchor {{ $page->selected('/') }}" href="/">Home</a></li>
 					<li><a class="fancy-anchor {{ $page->selected('about') }}" href="{{ $page->baseUrl }}/about">About</a></li>
 					<li><a class="fancy-anchor {{ $page->selected('projects') }}" href="{{ $page->baseUrl }}/projects">Projects</a></li>
 					<li><a class="fancy-anchor {{ $page->selected('posts') }}" href="{{ $page->baseUrl }}/posts">Blog</a></li>
 					<li><a class="fancy-anchor {{ $page->selected('contact') }}" href="{{ $page->baseUrl }}/contact">Contact</a></li>
-				</ul>				
+				</ul>
 			</nav>
 			
