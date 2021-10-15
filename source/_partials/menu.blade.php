b860013040011e860c55d5a8534e18cd280946f5
Andre Lewis <amalkong@gmail.com>
2021-10-15 11:55:10 -0500

    Update menu.blade.php

--- source/_partials/menu.blade.php
+++ source/_partials/menu.blade.php
@@ -2,7 +2,7 @@
 				<button class="nav-menu-button" onclick="document.querySelector('.main-nav').classList.toggle('toggled');"><i class="fa fa-bars"></i></button>
 				<button class="nav-close-button" onclick="document.querySelector('.main-nav').classList.remove('toggled');"><i class="fa fa-times"></i></button>
 				<ul>
-					<li><a class="fancy-anchor {{ $page->selected('/') }}" href="/">Home</a></li>
+					<li><a class="fancy-anchor {{ $page->selected('') }}" href="/">Home</a></li>
 					<li><a class="fancy-anchor {{ $page->selected('about') }}" href="{{ $page->baseUrl }}/about">About</a></li>
 					<li><a class="fancy-anchor {{ $page->selected('projects') }}" href="{{ $page->baseUrl }}/projects">Projects</a></li>
 					<li><a class="fancy-anchor {{ $page->selected('posts') }}" href="{{ $page->baseUrl }}/posts">Blog</a></li>
